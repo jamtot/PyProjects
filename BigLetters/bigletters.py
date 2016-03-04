18ce7bba9f58c3377fb8cfece3d7d2a46d52f67e
jamtot <jongrammor@gmail.com>
2016-03-04 22:39:01 +0000

    Added some error detection in the form of a try that changes offending letters to spaces.

--- BigLetters/bigletters.py
+++ BigLetters/bigletters.py
@@ -52,6 +52,8 @@ HASH=   [' * * ','*****',' * * ','*****',' * * ']
 AT=     [' *** ','*   *','*  **','*    ',' *** ']
 COLON=  ['     ',' **  ','     ',' **  ','     ']
 SEMIC=  ['     ',' **  ','     ',' **  ','  *  ']
+DQUOT=  ['** **','** **','     ','     ','     ']
+SQUOT=  ['  ** ','  ** ','     ','     ','     ']  
 
 
 symbols = {'A':A, 'B':B, 'C':C, 'D':D, 'E':E, 'F':F, 'G':G, 'H':H, 
@@ -60,18 +62,22 @@ symbols = {'A':A, 'B':B, 'C':C, 'D':D, 'E':E, 'F':F, 'G':G, 'H':H,
 ',':COMMA, '.':STOP, '=':EQUALS, '+':PLUS, '-':MINUS, '1':ONE, '2':TWO, 
 '3':THREE,'4':FOUR, '5':FIVE, '6':SIX, '7':SEVEN, '8':EIGHT, '9':NINE, 
 '0':ZERO, '?':QUEST, '!':EXCLAM, '\\':BACKSL, '/':FORSL, '#':HASH,
-'@':AT, ':':COLON, ';':SEMIC}
+'@':AT, ':':COLON, ';':SEMIC, '"':DQUOT, '\'':SQUOT}
 
-# no real error detection
+# added some error detection
 def makeBigLetters(input):
     capInput = list(input.upper())
     print '\n'
     for i in xrange(5):
         for j in xrange(len(capInput)):
+            try:
+                symbols[capInput[j]]
+            except KeyError:
+                capInput[j] = ' '
             line = symbols[capInput[j]][i]
             print line.replace('*', capInput[j]),
             if j == len(capInput)-1: print
     print '\n'
 
-msg = raw_input("Letter/s to make big: ")
+msg = raw_input("Make bigger: ")
 makeBigLetters(msg)
